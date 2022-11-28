b2bf8134fe7873c0cf1bfb7d3f778fc391933e14
Antoine <antoinexvipichard@gmail.com>
2022-11-28 15:31:18 +0100

    init 1.1

--- login.php
+++ login.php
@@ -1,5 +1,4 @@
-<?php 
-require_once "config.php"; 
+<?php require_once "config.php"; 
 $sql = "SELECT * FROM user WHERE email='".$_POST['email']."' AND password='".$_POST['password']."'"; 
 $pre = $pdo->prepare($sql); 
 $pre->execute();
